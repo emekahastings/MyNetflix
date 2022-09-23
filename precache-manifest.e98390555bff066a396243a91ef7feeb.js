63c448d6d22e0664cd6f50fb9af37e9c3799ff4f
Emeka Hastings <emekahastings1s@gmail.com>
2022-09-23 13:03:01 +0100

    Updates

---
+++ precache-manifest.e98390555bff066a396243a91ef7feeb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d903e60f5154278459b",
+    "url": "/static/css/main.542e996a.chunk.css"
+  },
+  {
+    "revision": "8d903e60f5154278459b",
+    "url": "/static/js/main.8d903e60.chunk.js"
+  },
+  {
+    "revision": "94421d0c55535ff39cc4",
+    "url": "/static/js/1.94421d0c.chunk.js"
+  },
+  {
+    "revision": "d5d85fb8d7b3cf0bd64d",
+    "url": "/static/js/2.d5d85fb8.chunk.js"
+  },
+  {
+    "revision": "935782d70e52593d67b3",
+    "url": "/static/js/runtime~main.935782d7.js"
+  },
+  {
+    "revision": "bc36a35049a39b89d6c6a6414bf7fd72",
+    "url": "/static/media/netflixcover copy.bc36a350.jpg"
+  },
+  {
+    "revision": "3da9bb52f0be925ec4f2d172af4ffe11",
+    "url": "/static/media/img3.3da9bb52.png"
+  },
+  {
+    "revision": "a48333d43c19612ed61987fa5dbff3c5",
+    "url": "/static/media/img2a.a48333d4.jpg"
+  },
+  {
+    "revision": "7796121ef2fa9f785df921c050c56231",
+    "url": "/static/media/sharpnetflixlogo.7796121e.png"
+  },
+  {
+    "revision": "587e040f20259792094901ca9739669e",
+    "url": "/static/media/img2b.587e040f.png"
+  },
+  {
+    "revision": "1c9dd740c64d9a0b6f7778199c021516",
+    "url": "/static/media/good.1c9dd740.webp"
+  },
+  {
+    "revision": "93c7e1d0ddc6399937c617856fc551fa",
+    "url": "/static/media/shortnetflixlogo.93c7e1d0.png"
+  },
+  {
+    "revision": "eb186c7772c3276583768d0ce2e09604",
+    "url": "/index.html"
+  }
+];
